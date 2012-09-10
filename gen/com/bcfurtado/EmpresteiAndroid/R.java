78e1ead2528291bf4599bd73607d6455343a55c0
Bruno Furtado <brunofurtadoc@gmail.com>
2012-09-10 02:11:03 -0300

    Primeira versão funcional da app

--- gen/com/bcfurtado/EmpresteiAndroid/R.java
+++ gen/com/bcfurtado/EmpresteiAndroid/R.java
@@ -12,12 +12,45 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int wood_panel=0x7f020001;
+    }
+    public static final class id {
+        public static final int TextViewObservacaoEdit=0x7f060014;
+        public static final int autoCompleteTextViewContato=0x7f060007;
+        public static final int buttonCadastrar=0x7f06000d;
+        public static final int buttonDevolver=0x7f060015;
+        public static final int buttonVerInformacoesContato=0x7f060016;
+        public static final int datePickerDevolucao=0x7f06000c;
+        public static final int editTextObjeto=0x7f060008;
+        public static final int editTextObservacao=0x7f06000a;
+        public static final int imageView1=0x7f06000f;
+        public static final int imageViewContato=0x7f060000;
+        public static final int listViewObjetosEmprestados=0x7f060003;
+        public static final int scrollView1=0x7f060006;
+        public static final int textMenuNovoEmprestimo=0x7f060004;
+        public static final int textViewCadastrarEmprestimo=0x7f060005;
+        public static final int textViewContato=0x7f060002;
+        public static final int textViewContatoEdit=0x7f060010;
+        public static final int textViewDevolucao=0x7f06000b;
+        public static final int textViewDtDevolucao=0x7f060012;
+        public static final int textViewDtDevolucaoEdit=0x7f060013;
+        public static final int textViewObjeto=0x7f060001;
+        public static final int textViewObjetoEdit=0x7f060011;
+        public static final int textViewObservacao=0x7f060009;
+        public static final int textViewVisualizarEmprestimo=0x7f06000e;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int emprestimo_item=0x7f030000;
+        public static final int home=0x7f030001;
+        public static final int home_menu=0x7f030002;
+        public static final int novo_emprestimo=0x7f030003;
+        public static final int visualizar_emprestimo=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int EmpresteiAndroid=0x7f050000;
     }
 }
